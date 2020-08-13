0952ea52e6c69819421ba9dd78d9b0a799e0c1e4
Kumar Nitesh <kumar.nitesh@endurance.com>
2020-08-12 23:39:49 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Something Awesome Arriving Here....
+      </header>
+    </div>
+  );
+}
+
+export default App;
